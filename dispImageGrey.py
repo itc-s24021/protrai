2fd661beade20c7c1eb0e263e4258b418920fbc7
itc-s24021 <s24021@std.it-college.ac.jp>
2024-07-01 07:52:42 +0900

    aa

--- dispImageGrey.py
+++ dispImageGrey.py
@@ -5,7 +5,7 @@ import tkinter as tk
 import tkinter.filedialog as fd
 import PIL.Image, PIL.ImageTk
 def dispPhoto(path):
-    newImage = PIL.Image.open(path).resize((300,300))
+    newImage = PIL.Image.open(path).convert("L").rotate(90).transpose(PIL.Image.FLIP_LEFT_RIGHT).resize((300,300))
     imageData = PIL.ImageTk.PhotoImage(newImage)
     imageLabel.configure(image = imageData)
     imageLabel.image = imageData
